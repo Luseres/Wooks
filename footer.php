de31c020279dd9778308933514166302435b4ae1
Wiebe Ranzijn <wiebe@justbetter.nl>
2020-05-29 12:37:53 +0200

    Make footer responsive

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
-	<footer>
+	<footer class="footer">
 		<div class="contents">
 			<div>
 				<p class="footer__title">Wooks</p>
-				<p class="footer__content">Wooks is a subscription company that brings you quality, New books to your doorstep every month.</p>
+				<p>Wooks is a subscription company that brings you quality, New books to your doorstep every month.</p>
 			</div>
 			<div>
 				<p class="footer__title">Links</p>
-				<a href="#" class="footer__link">Algemene Voorwaarden</a>
-				<a href="#" class="footer__link">Privacy Statement</a>
+				<a href="#">Algemene Voorwaarden</a>
+				<a href="#">Privacy Statement</a>
 			</div>
 			<span class="footer__copyright">Wooks Copyright 2020</span>
 		</div>
